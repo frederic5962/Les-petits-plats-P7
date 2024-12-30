6925f58d75d44d7213ea227d5459d9ed0d52ff78
Fred jeune DEV en devenir <fredstessy@outlook.fr>
2024-12-30 18:23:00 +0100

    finish page Home

--- js/script.js
+++ js/script.js
@@ -1,22 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const recipeCardsContainer = document.getElementById('recipe-cards');
 
   if (typeof recipes !== 'undefined') {
-      recipes.forEach(recipeData => {
-          const card = document.createElement('div');
-          card.className = 'card';
-          card.innerHTML = `
-              <img src="assets/${recipeData.image}" class="card-img-top" alt="${recipeData.name}">
-              <div class="card-body">
-                  <h5 class="card-title">${recipeData.name}</h5>
-                  <p class="card-text">Temps: ${recipeData.time} min</p>
-                  <p class="card-text">Ingrédients: ${recipeData.ingredients.map(ingredient => `${ingredient.ingredient}${ingredient.quantity ? ` (${ingredient.quantity}${ingredient.unit || ''})` : ''}`).join(', ')}</p>
-                  <p class="card-text">${recipeData.description}</p>
-              </div>
-          `;
-          recipeCardsContainer.appendChild(card);
-      });
+    recipes.forEach(recipeData => {
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = `
+                <div class="image-container">
+                    <img src="assets/medias/${
+                      recipeData.image
+                    }" class="card-img-top" alt="${recipeData.name}">
+                    <p class="card-time">${recipeData.time}min</p>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">${recipeData.name}</h5>
+                    <div class="card-recipe-title">RECETTE</div>
+                    <p class="card-recipe">${recipeData.description}</p>
+                    <div class="card-ingredients-title">INGRÉDIENTS</div>
+                    <div class="card-ingredients">
+                        ${recipeData.ingredients
+                          .map(
+                            ingredient => `
+                            <div class="ingredient-item">
+                                <div class="ingredient-name">${
+                                  ingredient.ingredient
+                                }</div>
+                                <div class="ingredient-quantity">${
+                                  ingredient.quantity
+                                    ? `${ingredient.quantity} ${ingredient.unit || ''}`
+                                    : ''
+                                }</div>
+                            </div>
+                        `
+                          )
+                          .join('')}
+                    </div>
+                </div>
+            `;
+      recipeCardsContainer.appendChild(card);
+    });
   } else {
-      console.error("Les données de recettes ne sont pas disponibles.");
+    console.error('Les données de recettes ne sont pas disponibles.');
   }
 });
